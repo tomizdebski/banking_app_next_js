97203283c3714d33f862fd9585f06fca57f3565a
tomizdebski <tomizdebski@gmail.com>
2024-08-24 14:57:21 +0200

    add_left_sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = ({ user }: any) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="flex mb-12 cursor-pointer items-center gap-2">
           <Image
             src="/icons/logo.svg"
             alt="Logo"
@@ -22,18 +22,35 @@ const Sidebar = ({ user }: any) => {
           <h1 className="sidebar-logo">Horizon</h1>
         </Link>
         {sidebarLinks.map((item) => {
-            const isActive = pathname === item.route;
+
+            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
             <Link
               href={item.route}
               key={item.label}
-              className={cn("sidebar-link")}
+              className={cn("sidebar-link", {'bg-bank-gradient': isActive})}
             >
-              {item.label}
+              <div className="relative size-6">
+                  <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({'brightness-[3] invert-0': isActive})}
+                  >
+                    
+                  </Image>
+
+              </div>
+              <p className={cn("sidebar-label", {"!text-white" : isActive})}>
+                {item.label}
+              </p>
             </Link>
           );
         })}
+        USER
       </nav>
+        FOOTER
     </section>
   );
 };
